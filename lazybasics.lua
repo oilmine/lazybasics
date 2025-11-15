e68ab6b8f518ff84823ea56462f1f56227fb4986
amateurist <investmentslips2@gmail.com>
2025-11-15 14:45:19 +0530

    Initial Commit
    bruh

---
+++ lazybasics.lua
@@ -0,0 +1,19 @@
+lb = {}
+
+function lb.add(x, y)
+    return x + y
+end
+
+function lb.subtract(x, y)
+    return x - y
+end
+
+function lb.multiply(x, y)
+    return x * y
+end
+
+function lb.divide(x, y)
+    return x / y
+end
+
+return lb
